b7caf407f8226bad9a75df8fd2e56f014f2c021c
korichi23 <falfolsara33@gmail.com>
2023-07-13 13:36:11 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main, print massage
- * return 0 for sucessful excution
+ * main - Entry point
+ *
+ * Decription: the program print with printf
+ *
+ * Return: Always 0 for sucessful excution
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return( 0);
+	return (0);
 }
