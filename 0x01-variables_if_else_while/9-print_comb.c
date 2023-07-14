1d460cc6bc22d7e1474032fc9e41c16e9bdfb987
korichi23 <falfolsara33@gmail.com>
2023-07-14 13:24:02 -0700

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Descreption: prints all possible combinations of digits
+ * Return: always 0 for successful excution
+ */
+int main(void)
+{
+	int d = 48;
+
+	while (d <= 57)
+	{
+		putchar(d);
+		if (d != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
