1a1729ecb85a9fac91067da83baeaef872a0ee5f
korichi23 <falfolsara33@gmail.com>
2023-07-18 10:44:08 -0700

    a

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs - function
+ * Descreption:computes the absolute value of an integer
+ * @n: the used number
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
+
