76e4abfdf8ffc087d46c653e49a1a13b44743dcb
korichi23 <falfolsara33@gmail.com>
2023-07-18 09:26:34 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function
+ * Descreption:prints the alphabet, in lowercase
+ * Return: void function
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
