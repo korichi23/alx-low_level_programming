0fb3b144f37288bd4416dc5da45a9dd2c5282242
korichi23 <falfolsara33@gmail.com>
2023-07-18 09:49:16 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,14 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char ch = 'a';
 
-	while(i < 10)
+	while (i < 10)
 	{
-		while(ch <= 'z')
+		while (ch <= 'z')
 		{
 			_putchar(ch);
+			ch++;
 		}
 		_putchar('\n');
+		i++;
 	}
 }
