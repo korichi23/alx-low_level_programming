5c7c3c572a73cf01e2df934766f041bb7caf5e37
korichi23 <falfolsara33@gmail.com>
2023-07-18 10:10:35 -0700

    a

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isalpha - function
+ * Descreption: checks for alphabetic character
+ * @c: the character to check
+ * Return:1 if c is a letter, lowercase or uppercase
+ * 0 otherwise
+ */
+int _isalpha(int c);
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
