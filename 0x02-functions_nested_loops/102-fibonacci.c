d0d614bdfd3a878a3c544d50a2389da01c344a81
korichi23 <falfolsara33@gmail.com>
2023-07-19 15:03:54 -0700

    a

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	unsigned long long int fib;
+	unsigned long int fib;
 	int fib1 = 0, fib2 = 1;
 	int i = 0;
 
@@ -15,7 +15,7 @@ int main(void)
 		fib = fib1 + fib2;
 		fib1 = fib2;
 		fib2 = fib;
-		printf("%llu, ", fib);
+		printf("%lu, ", fib);
 		i++;
 	}
 	puts("\n");
