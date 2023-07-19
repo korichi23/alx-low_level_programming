92af226c54b9854a441f0fb62aaffd0b11576135
korichi23 <falfolsara33@gmail.com>
2023-07-19 11:53:38 -0700

    a

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,29 +8,16 @@
  */
 void print_to_98(int n)
 {
-	while (n != 98)
+	int i;
+
+	if (n > 98)
 	{
-		int temp = n, i = 0, j = 0;
-		int nums[100];
-		while(temp > 10)
-		{
-			nums[i] = temp % 10;
-			temp /= 10;
-			i++;
-		}
-		nums[i] = temp;
-		for (j = strlen(nums); j <= 0; j--)
-		{
-			_putchar(nums[j]);
-			_putchar(',');
-			_putchar(' ');
-		}
-		if (n > 98)
-			n--;
-		else
-			n++;
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
+	else
+	{
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	}
 }
