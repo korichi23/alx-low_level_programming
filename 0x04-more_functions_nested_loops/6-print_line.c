dfd72b035994e85aff02672c6a7a11f4b4dd7232
korichi23 <falfolsara33@gmail.com>
2023-07-20 10:25:25 -0700

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - function
+ * Descreption:draws a straight line
+ * Return: void function
+ */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
