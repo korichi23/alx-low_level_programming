b2f6af5c046bbc5e1f57868b9c36129b6dfb6603
korichi23 <falfolsara33@gmail.com>
2023-07-20 13:22:13 -0700

    a

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,11 +13,11 @@ void print_square(int size)
 	{
 		for (i = 0; i < size ; i++)
 		{
-			for (j =0; j < size; j++)
+			for (j = 0; j < size; j++)
 			{
 				_putchar('#');
 			}
-			if (i != (size -1))
+			if (i != (size - 1))
 				_putchar('\n');
 		}
 	}
