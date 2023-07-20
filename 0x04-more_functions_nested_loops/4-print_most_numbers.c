73d62603f95297e6c95c94184a816b144532dd17
korichi23 <falfolsara33@gmail.com>
2023-07-20 09:46:19 -0700

    a

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#incluce "main.h"
+/**
+ * rint_most_numbers - function
+ * Descreption: prints the numbers except 2,4
+ * Return: void function
+ */
+void print_most_numbers(void)
+{
+	int ch = 48;
+
+	while (ch <= 57)
+	{
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
+	}
+	_putchar('\n');
+}
