a1ccd97b0733840ff300007bf8e74cc3eca593e1
korichi23 <falfolsara33@gmail.com>
2023-07-20 11:50:43 -0700

    a

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 && i % 5)
+		if (i % 3 == 0 && i % 5 == 0)
 			puts("FizzBuzz ");
-		else if (i % 3)
+		else if (i % 3 == 0)
 			puts("Fizz ");
-		else if (i % 5)
+		else if (i % 5 == 0)
 			puts("Buzz ");
 		else
 			printf("%d ", i);
