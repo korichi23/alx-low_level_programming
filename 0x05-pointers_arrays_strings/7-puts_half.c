6a4eaf41b587fcab08efa9948ac6123b7a0e4a6e
korichi23 <falfolsara33@gmail.com>
2023-07-24 15:43:18 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,10 +10,7 @@ void puts_half(char *str)
 {
 	int n, i;
 
-	if (strlen(str) % 2 != 0)
-		n = (strlen(str) - 1) / 2;
-	else
-		n = strlen(str) / 2;
+	n = (strlen(str) - 1) / 2;
 	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
