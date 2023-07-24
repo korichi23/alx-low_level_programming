37fc30525878ef4523dc28e6ace944768121144c
korichi23 <falfolsara33@gmail.com>
2023-07-24 14:41:47 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - function
+ * Descreption: prints every other character of a string
+ * @str:par
+ * Return: void function
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
