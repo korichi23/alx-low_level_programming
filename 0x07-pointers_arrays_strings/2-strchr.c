2fc87f09bf4aa8313ff3a5a2f6db79c9b9c94f10
korichi23 <falfolsara33@gmail.com>
2023-07-31 10:05:55 -0700

    a

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - function
+ * @s:first par
+ * @c:second par
+ * Return:pointer to the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	char *tmp = NULL;
+
+	while (*s)
+	{
+		if (*s == c)
+		{
+			tmp = s;
+			break;
+		}
+		++s;
+	}
+	return (tmp);
+}
