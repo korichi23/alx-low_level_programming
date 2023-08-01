aa3c794a35ad7bdfcf28d97d2dae47f734e9cbd6
korichi23 <falfolsara33@gmail.com>
2023-08-01 02:46:02 -0700

    a

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,14 +6,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i > 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		while (*a[i])
+		for (j = 0; j < 8; j++)
 		{
-			_putchar(*a[i]);
-			++a[i];
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
