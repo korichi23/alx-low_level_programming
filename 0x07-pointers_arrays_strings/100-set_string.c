44ef71903b2b413851c6196ec88ff077e86c3140
korichi23 <falfolsara33@gmail.com>
2023-08-01 07:22:44 -0700

    a

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main."
+/**
+ * set_string - function
+ * @s:first par
+ * @to :second par
+ * Return: void fuction
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,4 +11,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
 int compare(char *X, char *Y);
+void set_string(char **s, char *to);
 #endif 
