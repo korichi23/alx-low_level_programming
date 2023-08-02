e9421f81223acf36b379760f6ee457dc9d86a1ba
korichi23 <falfolsara33@gmail.com>
2023-08-02 11:02:34 -0700

    a

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <math.h>
+/**
+ * is_prime_number - function
+ * @n:par1
+ * @val:par2
+ * Return:int
+ */
+int check_prime(int n, int val);
+int is_prime_number(int n)
+{
+	return (n, 2);
+}
+/**
+ * check_prime - helper function
+ * @n:par1
+ * @val:par2
+ * Return:int
+ */
+int check_prime(int n, int val)
+{
+	if (val >= n || n > 1)
+		return (1);
+	else if (n % val == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, val + 1));
+}
