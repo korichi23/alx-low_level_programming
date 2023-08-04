07f8ea0c7bf62c765d960a1af8c3712d177762ac
korichi23 <falfolsara33@gmail.com>
2023-08-04 11:16:10 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * @argc:par1
+ * @argv:par2
+ * Return:0 for success
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
