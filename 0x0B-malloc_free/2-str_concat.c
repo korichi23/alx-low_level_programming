0480b054ce6d231622a13755c49ddb3a59e10515
korichi23 <falfolsara33@gmail.com>
2023-08-08 11:00:16 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,18 +9,18 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *str;
-	int len1, len2;
+	char *str, temp;
+	size_t len1, len2;
 
 	len1 = (s1 != NULL) ? strlen(s1) : 0;
 	len2 = (s2 != NULL) ? strlen(s2) : 0;
 	str = (char *)malloc(len1 + len2 + 1);
 	if (!str)
 		return (NULL);
-	
+	temp = str;
 	if (s1 != NULL)
 		strcpy(str, s1);
 	if (s2 != NULL)
 		strcpy(str, s2);
-	return (str);
+	return (temp);
 }
