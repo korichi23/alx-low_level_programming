26473ef6808c0ad927adcfe5d965218d4881eebb
korichi23 <falfolsara33@gmail.com>
2023-08-08 10:30:50 -0700

    a

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - function
+ * @s1:par1
+ * @s2:par2
+ * Return:pinter
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str, *temp;
+	int len;
+
+	len = strlen(s1) + strlen(s2) + 1;
+	str = (char *)malloc(sizeof(char) * len);
+	if (!str)
+		return (NULL);
+	temp = str;
+	while (*s1)
+	{
+		*str = *s1;
+		str++;
+		s1++;
+	}
+	while (*s2)
+	{
+		*str = *s2;
+		str++;
+		s2++;
+	}
+	*str = '\0';
+	return (temp);
+}
