be6dfe607963f906031de1c759e39c3afd688064
korichi23 <falfolsara33@gmail.com>
2023-08-08 09:44:49 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strdup - function
+ * @str:par
+ * Return:a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *s, *temp;
+
+	s = (char *)malloc(sizeof(str))
+	if (!s)
+		return (NULL);
+	temp = str;
+	while (*str)
+	{
+		*s = *str;
+		str++;
+		s++;
+	}
+	return (temp);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_   
 #define MAIN_H_
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
 
