e098220e656cdffb811c2cf68628318a150a8809
korichi23 <falfolsara33@gmail.com>
2023-08-10 10:53:15 -0700

    a

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - function
+ * @s1:par1
+ * @s2:par2
+ * @n:par3
+ * Return:pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	int len = strlen(s1) + n;
+
+	str = (char *)malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	str = strncat(s1, s2, n);
+	str[len] = '\0';
+	return (str);
+}
