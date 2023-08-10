77ccde81fc83cc77acc1ce81d0b42e2f7347ff64
korichi23 <falfolsara33@gmail.com>
2023-08-10 13:02:13 -0700

    a

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 	char *temp;
-	int i;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	temp = (char *)ptr;
 	for (i = 0; i < nmemb * size; i++)
 	{
-		temp[i] = 48;
+		temp[i] = 0;
 	}
 	return (ptr);
 }
