c29f23b67bdf85663a6817e426860ae87a894245
korichi23 <falfolsara33@gmail.com>
2023-08-14 10:09:43 -0700

    a

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef -OBJECT_LIKE_MACRO_H_   
-#define -OBJECT_LIKE_MACRO_H_
+#ifndef _OBJECT_LIKE_MACRO_H_   
+#define _OBJECT_LIKE_MACRO_H_
 
 #define SIZE 1024
 
-#endif // 0-OBJECT_LIKE_MACRO_H_
+#endif 
