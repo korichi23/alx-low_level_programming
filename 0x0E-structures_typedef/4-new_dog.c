5688dc91becbad7b7f333e098523b1879b7f8911
korichi23 <falfolsara33@gmail.com>
2023-08-14 15:14:37 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - function
  * @name:par1
@@ -11,12 +12,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	if (!name || age < 0 || !owner)
 		return (NULL);
-	dog_t *dog = malloc(sizeof(dog_t));
+	dog_t *Ndog;
 
-	if (dog)
+	Ndog =  malloc(sizeof(dog_t));
+	if (Ndog)
 		return (NULL);
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
-	return (dog);
+
+	Ndog->name = strdup(name);
+	if (Ndog->name == NULL)
+	{
+		free(Ndog->name);
+		return (NULL);
+	}
+	Ndog->age = age;
+	Ndog->owner = strdup(owner);
+	if (Ndog->owner)
+	{
+		free(Ndog->owner);
+		return (NULL);
+	}
+	return (Ndog);
 }
