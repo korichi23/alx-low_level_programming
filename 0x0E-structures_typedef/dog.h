d4faca546e641f5e083e74027692a6fc064dca28
korichi23 <falfolsara33@gmail.com>
2023-08-14 14:26:59 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,7 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+void print_dog(struct dog *d)u;
+typedef struct dog dog_t;
 
 #endif
